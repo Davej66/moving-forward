614c63ed334fd2febd10b1baa78568dee9f02848
Davej66 <daijacobs@gmail.com>
2022-04-15 15:07:11 +0000

    Adding initial subscription settings

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from djstripe.models import Product
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html',{
+        'products': Product.objects.all()
+    })
