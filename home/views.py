fe2488346a32a99471950200c698ba88e35394bc
Davej66 <daijacobs@gmail.com>
2022-04-15 20:29:07 +0000

    Updating home views.py

--- home/views.py
+++ home/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from djstripe.models import Product
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html',{
-        'products': Product.objects.all()
-    })
+    return render(request, 'home/index.html',
+    )
