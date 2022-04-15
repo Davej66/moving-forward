41fd3b69e77e16ff3736d03d5f6c9bfecc180e28
Davej66 <daijacobs@gmail.com>
2022-04-15 21:03:01 +0000

    Correcting typo's in moving_forward/urls.py

--- moving_forward/urls.py
+++ moving_forward/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('profile/', include('profiles.urls')),
     path('checkout/', include('checkout.urls')),
-    path("stripe/", include("djstripe.urls", namespace="djstripe")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
