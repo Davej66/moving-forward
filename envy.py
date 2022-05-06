82d2633212d07a850e8b31f88952a0d4d2fbf4ed
Davej66 <daijacobs@gmail.com>
2022-05-06 16:15:29 +0000

    Updated styling

--- envy.py
+++ envy.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["SECRET_KEY"] = "*tgp%fnlj2u8wfm4v)5*y-dn=fpn3kdsw3e=7g^)3!xl28=d=+"
+os.environ["SECRET_KEY"] = "s9%cie4@i_y*t%a5cnuhl-#f31(q#-1h=k&pjetxc4#msx=0vz"
 os.environ["DEV"] = "1"
 os.environ["HOSTNAME"] = "0.0.0.0"
 os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51KkpyKHylLmfaYUcxfeNFtQqXC9FUzKdlgfLbvobMIMdyK8RDNk199dzBVPlXZgTLWSOkxoBtNRKoU5fYwKwy5w500szJ6Nr34"
