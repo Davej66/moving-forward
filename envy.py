8bc703c6ea9d6e0b13504ff3912ec90c046e3fee
Davej66 <daijacobs@gmail.com>
2022-05-08 06:50:53 +0000

    Correcting typo's

--- envy.py
+++ envy.py
@@ -6,4 +6,4 @@ os.environ["HOSTNAME"] = "0.0.0.0"
 os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51KkpyKHylLmfaYUcxfeNFtQqXC9FUzKdlgfLbvobMIMdyK8RDNk199dzBVPlXZgTLWSOkxoBtNRKoU5fYwKwy5w500szJ6Nr34"
 os.environ["STRIPE_SECRET_KEY"] = "sk_test_51KkpyKHylLmfaYUc7ffStKCn79yNHRKAZkUbVsXZITGmxyVfiXQtKI5mljwMu8A3XyPvR4DHZFm1FtGXCwiX8gxJ00rtbPJfo7"
 os.environ["DATABASE_URL"] = "moving-forward"
-os.environ["DEBUG"] = False
+os.environ["DEBUG"] = True
