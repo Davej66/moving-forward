52c249a78b5c09af8ba9b4996c18a0e488e5ca4b
Davej66 <daijacobs@gmail.com>
2022-07-27 18:21:11 +0000

    Correcting env.py file

--- envy.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = "s9%cie4@i_y*t%a5cnuhl-#f31(q#-1h=k&pjetxc4#msx=0vz"
-os.environ["DEV"] = "1"
-os.environ["HOSTNAME"] = "0.0.0.0"
-os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51KkpyKHylLmfaYUcxfeNFtQqXC9FUzKdlgfLbvobMIMdyK8RDNk199dzBVPlXZgTLWSOkxoBtNRKoU5fYwKwy5w500szJ6Nr34"
-os.environ["STRIPE_SECRET_KEY"] = "sk_test_51KkpyKHylLmfaYUc7ffStKCn79yNHRKAZkUbVsXZITGmxyVfiXQtKI5mljwMu8A3XyPvR4DHZFm1FtGXCwiX8gxJ00rtbPJfo7"
-os.environ["DATABASE_URL"] = "moving-forward"
-os.environ["DEBUG"] = True
